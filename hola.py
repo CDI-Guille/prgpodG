297920657b3abbcf870370400d3e5eb3dcf94950
CDI-Guille <a01733608@itesm.mx>
2020-10-27 08:53:01 +0000

    Agrego archivo hola.py

--- hola.py
+++ hola.py
@@ -1,8 +1,10 @@
-h=("Hola mundo!!! Pd. No se que hago XD")
-print(h)
+print("Hola mundo!!! Pd. No se que hago XD")
 
-lista=[1,4,7,2]
-lista.append(10)
-
-print(lista[2])
-
+lista=[1,4,7,2,10]
+for recorrido in range(1,len(lista)):
+    for posicion in range(len(lista)-recorrido):
+        if lista[posicion]>lista[posicion+1]:
+            temp=lista[posicion]
+            lista[posicion]=lista[posicion+1]
+            lista[posicion+1]=temp
+print(lista)
